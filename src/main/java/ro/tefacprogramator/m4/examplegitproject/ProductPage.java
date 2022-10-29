b7e888f813836512718a2a0e973055c91555b1b4
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-10-29 10:42:01 +0300

    Added the product page

---
+++ src/main/java/ro/tefacprogramator/m4/examplegitproject/ProductPage.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.examplegitproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductPage {
+    
+    @GetMapping("/product")
+    public String product() {
+        return "Product details";
+    }
+}
