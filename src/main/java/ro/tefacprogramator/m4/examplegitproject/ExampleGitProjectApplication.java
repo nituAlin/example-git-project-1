0f03296940a0d648fe070a0d2ea5954d9eb3ecaf
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-10-29 10:20:23 +0300

    Initial commit

---
+++ src/main/java/ro/tefacprogramator/m4/examplegitproject/ExampleGitProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.examplegitproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleGitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/tefacprogramator/m4/examplegitproject/ExampleGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.tefacprogramator.m4.examplegitproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
