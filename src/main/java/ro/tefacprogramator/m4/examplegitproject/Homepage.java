a370686c6f0f990b9206a8930af0bc484c5906f6
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-10-29 10:27:04 +0300

    Added the homepage

---
+++ src/main/java/ro/tefacprogramator/m4/examplegitproject/Homepage.java
@@ -0,0 +1,14 @@
+package ro.tefacprogramator.m4.examplegitproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homepage {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to my app";
+    }
+
+}
